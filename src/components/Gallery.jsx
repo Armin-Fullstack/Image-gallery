eb9dcdf29ac052de9a84600cef3c0366802ab53f
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-04 15:06:40 +0330

    Implemented onShowDetailsHandler

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,28 +5,34 @@ import { useState } from "react"
 
 const Gallery = () => {
     const [index , setIndex] = useState(0)
+    const [showDetails , setShowDetails] = useState(false)
 
     const onNextButtonHandler = () => {
         setIndex(index + 1)
     }
 
+    const onShowDetailsHandler = () => {
+        setShowDetails(!showDetails)
+    }
+
+
     let sculpture = sculptureList[index]
 
     return (
 
-        <div className="flex flex-col rounded-xl bg-gray-400 p-3">
+        <div className="flex flex-col rounded-xl bg-gray-400 p-3 max-w-lg mx-5 md:mx-0">
             <div>
-                <img src="./src/assets/image/images.jpg" alt=""  className="rounded-t-xl w-full" />
+                <img src={sculpture.url} alt={sculpture.alt}  className="rounded-t-xl w-full" />
             </div>
 
         <div className="bg-white p-3 pb-5 space-y-5 rounded-b-xl">
             <div className="flex justify-between items-center">
-            <h1 className="text-xl font-bold">Title</h1>
+            <h1 className="text-xl font-bold">{sculpture.name}</h1>
             <p className="text-md">({index + 1} of {sculptureList.length})</p>
             </div>
-            <p className="text-md leading-8">Description</p>
+            <p className="text-md leading-8">{showDetails && sculpture.description }</p>
             <div className="flex justify-between items-center">
-                <ShowDetailsButton/>
+                <ShowDetailsButton onShowDetailsHandler={onShowDetailsHandler}/>
                 <NextButton onNextButtonHandler={onNextButtonHandler}/>
             </div>
         </div>
--- src/components/ShowDetailsButton.jsx
+++ src/components/ShowDetailsButton.jsx
@@ -1,8 +1,8 @@
 
-const ShowDetailsButton = () => {
+const ShowDetailsButton = ({onShowDetailsHandler}) => {
     return (
-        
-        <button className="bg-blue-600 px-5 py-2 rounded-lg text-white hover:opacity-80 duration-300">Show Details</button>
+
+        <button onClick={onShowDetailsHandler} className="bg-blue-600 px-5 py-2 rounded-lg text-white hover:opacity-80 duration-300">Show Details</button>
     )
 }
 
